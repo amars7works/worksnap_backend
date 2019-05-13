33bae7af1f25cbc311f428ce47b3bfd3ecd669da
Vikram Palnati <vikramp@s7works.io>
2019-05-13 18:43:55 +0530

    added user_daily_report_mail

--- reports_2/urls.py
+++ reports_2/urls.py
@@ -3,7 +3,7 @@ from .views import ApplyLeaveView, leave_details
 
 
 urlpatterns = [
-	url(r'^api/apply_leave/', ApplyLeaveView.as_view(), name='user apply for leave'),
-	url(r'^api/leave/',leave_details.as_view()),
-	url(r'^api/leave/(?P<id>\d+)/$', leave_details.as_view()),
+	url(r'^apply_leave/', ApplyLeaveView.as_view(), name='user apply for leave'),
+	url(r'^get_requests/',leave_details.as_view()),
+	# url(r'^get_requests/(?P<id>\d+)/$', leave_details.as_view()),
 ]
